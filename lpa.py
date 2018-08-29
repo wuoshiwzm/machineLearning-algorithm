6f6898767a5974fe966d8f9977bddea08c987464
wuoshiwzm <wuoshiwzm@hotmail.com>
2018-08-29 19:47:58 +0800

    8-29

---
+++ lpa.py
@@ -0,0 +1,70 @@
+# 标签传递算法 LPA
+import  random
+
+def label_propagation(data, a):
+    # 转移矩阵
+    p = transition_matrix(data)
+    m = len(data)#样本数目
+    n = len(data[0])#样本维度
+    for times in range(1000):#迭代1000次
+        # i从a没有标记的一直到m-1,计算i号的样本，它的标签是谁，谁应该把标签传递给第i号
+        for i in range(a, m):
+
+            j = calc_label(p,i)
+            label = data[j][n-1]#data[j]的标签
+            if label>0
+                data[i][n-1] = label
+
+
+def calc_label(p,i): #p:概率 pij:从顶点i转移到顶点j的概率
+    n = len(p[i])
+    k = random.random() #k belongsto [0,1)
+    r = n-1
+    for j in range(n):
+        if p[i][j] >k: #如果Pij的转移概率大于随机数k，就把标签返回
+            r=j
+            break
+    return r
+
+
+
+
+
+    #
+    #         label = 0
+    #         for j in range(0, m):#把j的标签传递给i 如果j没有标签就不给
+    #             if p[i][j] > 0 and data[j][n - 1] != 0:
+    #                 label += p[i][j] * data[j][n - 1]
+    #         if in_test(data[i]):
+    #             print(times, data[i][n - 1], label)
+    #         if label > 0:
+    #             data[i][n - 1] = round_label(label)
+    # for i in range(a, m):
+    #     data[i][n - 1] = max_label(data[i][n - 1])
+    #
+    # return
+
+
+# 转移矩阵 与laplace矩阵很像
+def transition_matrix(data):
+    m = len(data)
+    p = [[] for x in range(m)]
+    for i in range(m):
+        p[i] = [0 for x in range(m)]
+    nearest = [0 for x in range(neigbor)]
+
+    for i in range(m):
+        s = 0
+        zero_list(nearest)
+        for j in range(m):
+            if i != j:
+                p[i][j] = similar(data, i, j)
+            else:
+                p[i][j] = 0
+            if is_neighbor(p[i][j], nearest):
+                s += p[i][j]
+            else:
+                p[i][j] = 0
+        for j in range(m):
+            p[i][j] /= s
+    return p
--- spectral_cluster.py
+++ spectral_cluster.py
@@ -3,6 +3,8 @@
 # 引用
 import k_means
 
+k = 40
+
 
 def spectral_cluster(data):
     # 生成laplace 矩阵
@@ -16,61 +18,63 @@ def spectral_cluster(data):
     m = len(data)
     # eg_data就是教程中的u
     eg_data = [[] for x in range(m)]
-    for i in range(m)
+    for i in range(m):
         # u中的第i行 ：y[i]
         eg_data[i] = [0 for x in range(k)]
 
         # 选取前K个特征征
-        for j in range(k)
+        for j in range(k):
             eg_data[i][j] = eg_vectors[i][j]
     return k_means(eg_data)
 
 
 # laplace矩阵生成
 def laplace_matrix(data):
-
-
     # m个样本
     m = len(data)
     w = [[] for x in range(m)]
-    #初始化w矩阵
+    # 初始化w矩阵
     for i in range(m):
         w[i] = [0 for x in range(m)]
-    #最近
+    # 最近
     nearest = [0 for x in range(neighbor)]
 
+    #生成W矩阵
     for i in range(m):
         zero_list(nearest)
-        for j in range(i+1,m):
-            #计算第i和第j个相似度
-            w[i][j] = similar(data,i,j)
-            #如果不是前r个紧邻，就清零
-            if not is_neighbor(w[i][j],nearest):
+        for j in range(i + 1, m):
+            # 计算第i和第j个相似度
+            w[i][j] = similar(data, i, j)
+            # 如果不是前r个紧邻，就清零
+            if not is_neighbor(w[i][j], nearest):
                 w[i][j] = 0
-            #否则就赋值，并对角化赋值
+            # 否则就赋值，并对角化赋值
             w[j][i] = w[i][j]
         w[i][i] = 0
+    #生成   Lrw=D^(-1)(D-W) = I - D^(-1)W
     for i in range(m):
-        #求度s
-        s=0
+        # 求度s
+        s = 0
+        #di为i上所有的w之和
         for j in range(m):
-            s+=w[i][j]
-        if s==0:
-            print('矩阵第',i,'行全为0')
+            s += w[i][j]
+        if s == 0:
+            print('矩阵第', i, '行全为0')
             continue
-        #随机游走的laplace矩阵
+        # 随机游走的laplace矩阵
         for j in range(m):
             w[i][j] /= s
             w[i][j] = -w[i][j]
 
-        #单位阵主对角线为1
+        # 单位阵主对角线为1
         w[i][i] += 1
     return w
 
 
-#算相似度
+# 算相似度
 def is_similar(data, cluster_center):
     return
+
 
 #
 def nearest(data, cluster_center):
